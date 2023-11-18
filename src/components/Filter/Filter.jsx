bc118a17a3b20381570ea8945f7183b871f18d64
Sergey Solodovnyk <xxxsergxxx1990@gmail.com>
2023-11-18 15:09:27 +0200

    //

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,11 +1,17 @@
-import { Component } from 'react';
+
 import PropTypes from 'prop-types';
 
 import s from '../Filter/Filter.module.css';
 
-export class Filter extends Component {
-  render() {
-    const { filter, addFilter } = this.props;
+
+export const Filter = ({filter, addFilter})=>{
+
+
+  const filters = (e) => {
+    addFilter(e.target.value);
+  };
+ 
+
     return (
       <div className={s.filter}>
         <input
@@ -13,13 +19,39 @@ export class Filter extends Component {
           name="filter"
           className={s.filter__input}
           value={filter}
-          onChange={addFilter}
+          onChange={filters}
           placeholder="Enter name"
         />
       </div>
     );
-  }
+
 }
+
+
+
+
+
+
+
+
+
+// export class Filter extends Component {
+//   render() {
+//     const { filter, addFilter } = this.props;
+//     return (
+//       <div className={s.filter}>
+//         <input
+//           type="text"
+//           name="filter"
+//           className={s.filter__input}
+//           value={filter}
+//           onChange={addFilter}
+//           placeholder="Enter name"
+//         />
+//       </div>
+//     );
+//   }
+// }
 
 Filter.propTypes = {
   filter: PropTypes.string.isRequired,
